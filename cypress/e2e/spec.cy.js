eb161aa5681e5a7760ba60f4e31385fce8a309f6
Andrew Reiblein <andrewreiblein@Andrews-MacBook-Pro.local>
2024-07-26 12:34:45 +0200

    Updating more specs

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,20 +6,41 @@ describe('template spec', () => {
     cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
   });  
 
-  it('Verify Radio button', () => {
+  it('Radio Buttons', () => {
+    // Clicking on Radio Button
     cy.get('[for="radio1"] > .radioButton').click()
+    // Verifying checked buttons
     cy.get('[for="radio1"] > .radioButton').should('be.checked')
     cy.get('[for="radio2"] > .radioButton').should('not.be.checked')
     cy.get('[for="radio3"] > .radioButton').should('not.be.checked')
   })
 
-  it('Verifying suggestion', () => {
+  it('Suggestions', () => {
+    // Testing Auto Complete 
     cy.get('#autocomplete').click().type("South")
     cy.contains("South Africa").click()
     cy.get('#autocomplete')
     .clear()
+    // Testing Dropdown menu
     .type("Republic")
     .wait(2000)
     cy.get("#ui-id-1").click({force:true})
+    // Need to come back to this 
+  })
+
+  it('Checkboxes', () => {
+    // Check all boxes and validate 
+
+    // Uncheck first box and validate 
+
+  })
+  it('Show/hide', () => {
+
+  })
+  it('Web Table Fixed Header ', () => {
+
+  })
+  it('iFrame', () => {
+
   })
 })
